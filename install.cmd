bd72ef1dfdfe63b607d0aaffd13893bfbf6678f3
Alexandr <maximchik.alexandr@yandex.ru>
2023-09-18 23:16:55 +0300

    Add windows installation file, fix linux installation file

---
+++ install.cmd
@@ -0,0 +1,11 @@
+echo POSTGRES_DB=test_db > .env
+echo POSTGRES_USER=postgres >> .env
+echo POSTGRES_PASSWORD=123456 >> .env
+echo DB_HOST=postgres_db >> .env
+echo DB_PORT=5432 >> .env
+echo DB_OUT_PORT=5434 >> .env
+echo SQLALCHEMY_LOG_LEVEL=CRITICAL >> .env
+echo TELEGRAM_TOKEN= >> .env
+
+docker compose up -d --build && \
+docker exec -it pc_app alembic upgrade head
